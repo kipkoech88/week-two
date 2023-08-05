185951798e4b9f705bd3952c8575a63c9cef28cf
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-05 16:45:28 +0300

    added usestate hook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const InputField = ()=>{
-    return <input type="text" placeholder="enter some text"/>
+    const [input, setInput] = useState("");
+    const [historyList, setHistoryList]=useState([]);
+
+    return (
+        <div>
+            <input type="text" placeholder="enter some text"
+        onChange={(e)=>{
+            console.log(e.target.value)
+            setInput(e.target.value)
+            setHistoryList([...historyList, e.target.value]);
+        }}
+    />
+    <div>
+    {input}
+    </div>
+    <hr/><br/>
+    <ul>
+        {historyList.map((ob)=>{
+            return <div>{ob}</div>
+        })}
+    </ul>
+        </div>
+    )
 }
 
 export default InputField
