8965a6e04f550e4d430ad6dbcc701155d3685621
kipkoech88 <hoseakipkoech88@gmail.com>
2023-08-07 13:22:56 +0300

    added header

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='jumbotron jumbotronheight'>
+    <div className='row'>
+        <div className='col-12 col-sm-4 text-center'>
+            <h6>October, 07 2023</h6>
+            <h6>Meru, Kenya</h6>
+        </div>
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header
